a6e376e151b24e4a9a231bd2e81b38365775acdb
NghiaDuy2109 <laiduynghia21@gmail.com>
2022-03-24 12:29:54 +0700

    exercise1

--- homework/day01/src/ex1_b.java
+++ homework/day01/src/ex1_b.java
@@ -3,5 +3,6 @@ public class ex1_b {
         int i = 0;
 //        System.out.println("i++ có giá trị: "+ i++);
         System.out.println("++i có giá trị: "+ ++i);
+//        Khi in ra màn hình i++ sẽ in ra gtri của biến i ban đầu, còn ++i sẽ tăng gtri của biến i thêm 1 r sẽ in gtri đó ra màn hình
     }
 }
