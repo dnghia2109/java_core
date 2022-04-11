043c9976cbb95535133d514c55cd9597cca1543c
NghiaDuy2109 <laiduynghia21@gmail.com>
2022-04-11 22:53:38 +0700

    demo_book

--- demo_book/src/model/Book.java
+++ demo_book/src/model/Book.java
@@ -8,12 +8,16 @@ public class Book {
     public int id, publishingYear;
     public String title, description, author, category;
 
+    @Override
     public String toString() {
         return "ID sách: " + id + " - Tên sách: " + title + " - Mô tả: " + description +
                 " - Thể loại: " + category + " - Tác giả: " + author + " - Năm xuất bản: " + publishingYear;
     }
+
 //    public void showBook(){
 //        System.out.println("ID sách: " + id + " - Tên sách: " + title + " - Mô tả: " + description +
 //                " - Thể loại: " + category + " - Tác giả: " + author + " - Năm xuất bản: " + publishingYear);
 //    }
+
+
 }
