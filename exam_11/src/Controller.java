a9f366811de9a9c8f0b0a2c424b9dcac84d3599d
NghiaDuy2109 <laiduynghia21@gmail.com>
2022-04-21 20:04:06 +0700

    exam_11

---
+++ exam_11/src/Controller.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Controller {
+    public void mainMenu(){
+        System.out.println("====== Main menu ======");
+        System.out.println("1. Đăng nhập.");
+        System.out.println("2. Đăng ký.");
+    }
+
+    public static void signInCompleteMenu(){
+        System.out.println("1 - Thay đổi username");
+        System.out.println("2 - Thay đổi email");
+        System.out.println("3 - Thay đổi mật khẩu");
+        System.out.println("4 - Đăng xuất");
+        System.out.println("5 - Thoát");
+    }
+
+    public void mainController(){
+        Scanner sc = new Scanner(System.in);
+        boolean check = true;
+        ArrayList<User> listUser = new ArrayList<>();
+        UserService service = new UserService();
+
+        do {
+            mainMenu();
+            int option = Integer.parseInt(sc.nextLine());
+            switch (option){
+                case 1:
+                    break;
+                case 2:
+                    break;
+            }
+        } while (check);
+
+    }
+}
+
+
---
+++ exam_11/src/UserService.java
@@ -0,0 +1,269 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserService {
+
+    public void signIn(ArrayList<User> userArrayList){
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true, check = true;
+        User user = new User();
+        do {
+            System.out.println("- Username:");
+            String checkUsername = sc.nextLine();
+            for (User user1 : userArrayList){
+                if (user1.getUserName().equals(checkUsername)){
+                    user = user1;
+                    flag = false;
+                }else {
+                    System.out.println("Kiểm tra lại Username.");
+                }
+            }
+        }while (flag);
+
+        System.out.println("- Password:");
+        String checkPassword = sc.nextLine();
+        do {
+            if (user.getPassword().equals(checkPassword)){
+                System.out.println("Chào mừng " + user.getUserName() + ", bạn có thể thực hiện" + " các công việc sau:");
+                signInComplete(userArrayList, user);
+                flag = false;
+            }else {
+                int option = Integer.parseInt(sc.nextLine());
+                switch (option){
+                    case 1:
+                        signIn(userArrayList);
+                        break;
+                    case 2:
+
+                }
+            }
+        }while (flag);
+    }
+
+
+    // Ktra username khi đăng nhập
+    public User checkUserNameSignIn(ArrayList<User> userArrayList){
+        User user =  new User();
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        do {
+            System.out.println("- Username:");
+            String checkUsername = sc.nextLine();
+            for (User user1 : userArrayList){
+                if (user1.getUserName().equals(checkUsername)){
+                    user = user1;
+                    flag  = false;
+                }
+            }
+        }while (flag);
+
+        return user;
+    }
+
+    // Ktra password khi đăng nhập
+    public static void checkPasswordSignIn(ArrayList<User> userArrayList, User user){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("- Password:");
+        String checkPassword = sc.nextLine();
+        boolean flag = true, check = true;
+        do {
+            if (user.getPassword().equals(checkPassword)){
+                System.out.println("Chào mừng " + user.getUserName() + ", bạn có thể thực hiện các công việc sau:");
+                signInComplete(userArrayList, user);
+                flag = false;
+            }else {
+
+            }
+        }while (flag);
+
+    }
+
+    // Đăng nhập thành công
+    public static void signInComplete(ArrayList<User> userArrayList, User user){
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        do {
+            Controller.signInCompleteMenu();
+            int option = Integer.parseInt(sc.nextLine());
+            switch (option){
+                case 1:
+                    checkNewUserName(userArrayList, user);
+                    break;
+                case 2:
+                    checkNewEmail(userArrayList, user);
+                    break;
+                case 3:
+                    checkNewPassword(userArrayList, user);
+                    break;
+                case 4:
+                    flag = false;
+                    break;
+                case 5:
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Chọn từ 1-3.");
+                    break;
+            }
+        }while (flag);
+    }
+
+    // Đăng ký
+//    public User createNewUser(){
+//        Scanner sc = new Scanner(System.in);
+//        User us = new User();
+//        System.out.println("- Username:");
+//        us.setUserName(sc.nextLine());
+//
+//        boolean flag = true;
+//        while (flag){
+//            System.out.println("- Email:");
+//            us.setEmail(sc.nextLine());
+//            String regexEmail = "^[a-zA-Z][\\\\w-]+@([\\\\w]+\\\\.[\\\\w]+|[\\\\w]+\\\\.[\\\\w]{2,}\\\\.[\\\\w]{2,})$";
+//            if (Pattern.matches(regexEmail, us.getEmail())){
+//                flag = false;
+//            }else {
+//                System.out.println("Nhập email đúng định dạng");
+//            }
+//        }
+//
+//        while (flag){
+//            System.out.println("- Password:");
+//            us.setPassword(sc.nextLine());
+//            String regexPassword = "";
+//            if (Pattern.matches(regexPassword, us.getPassword())){
+//                flag = false;
+//            }else {
+//                System.out.println("Nhập mật khẩu dài từ 7 đến 15 ký tự, chứa ít nhất 1 ký tự in hoa, 1 ký tự đặc biệt");
+//            }
+//        }
+//
+//        return us;
+//    }
+
+
+    // Check username khi đăng ký + Thay đổi username
+    public static void checkNewUserName(ArrayList<User> userArrayList, User user){
+        Scanner sc = new Scanner(System.in);
+        boolean  flag = false;
+        do {
+            System.out.println("- Nhập Username:");
+            String checkUserName = sc.nextLine();
+            if (userArrayList.size() > 0){
+                for (User user1 : userArrayList){
+                    if (user1.getUserName().equals(checkUserName)){
+                        System.out.println("Username đã tồn tại! Vui lòng sử dụng Username khác.");
+                        flag = true;
+                    }else {
+                        user.setUserName(checkUserName);
+                        flag = false;
+                    }
+                }
+            }else {
+                user.setUserName(checkUserName);
+                flag = false;
+            }
+
+        }while (flag);
+    }
+
+
+    // Check Email khi đăng ký + Thay đổi email
+    public static void checkNewEmail(ArrayList<User> userArrayList, User user){
+        Scanner sc = new Scanner(System.in);
+        boolean  flag = false;
+        do {
+            System.out.println("- Nhập email:");
+            String regexEmail = "^[a-zA-Z][\\\\w-]+@([\\\\w]+\\\\.[\\\\w]+|[\\\\w]+" +
+                    "\\\\.[\\\\w]{2,}\\\\.[\\\\w]{2,})$";
+
+            String checkEmail = sc.nextLine();
+            if (Pattern.matches(regexEmail, checkEmail)){
+                if (userArrayList.size() > 0 ){
+                    for (User user1 : userArrayList){
+                        if (user1.getEmail().equals(checkEmail)){
+                            System.out.println("Email đã tổồn tại! Vui lòng sử dụng Email khác.");
+                            flag = true;
+                            //break;
+                        }else {
+                            user.setEmail(checkEmail);
+                            flag = false;
+                        }
+                    }
+                }else {
+                    user.setEmail(checkEmail);
+                    flag = false;
+                }
+            }else {
+                System.out.println("Nhập email đúng định dạng");
+                flag = true;
+//                break;
+            }
+        }while (flag);
+    }
+
+
+    // Check password khi đăng ký +Thay đổi password
+    public static void checkNewPassword(ArrayList<User> userArrayList, User user){
+        Scanner sc = new Scanner(System.in);
+        boolean  flag = false;
+        do {
+            System.out.println("- Nhập password:");
+            String regexPassword = "^(?=.*[a-zA-Z])(?=.*[\\.\\,\\(\\_]).{7,15}$";
+            String checkPassword = sc.nextLine();
+            if (Pattern.matches(regexPassword, checkPassword)){
+                user.setPassword(checkPassword);
+                flag =  false;
+            }else {
+                System.out.println("Nhập password đúng định dạng dài từ 7 " +
+                        "đến 15 ký tự, chứa ít nhất 1 ký tự in hoa, 1 ký tự đặc biệt");
+                flag = true;
+//                break;
+            }
+        }while (flag);
+
+    }
+
+
+
+
+
+
+
+
+//    public void updateUsername(User user){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("- Nhập username cập nhật mới:");
+//        String updateUsername = sc.nextLine();
+//        user.setUserName(updateUsername);
+//    }
+//
+//    public void updateEmail(User user){
+//        Scanner sc = new Scanner(System.in);
+//        boolean  flag = false;
+//        System.out.println("- Nhập email cập nhật mới:");
+//        String updateEmail = sc.nextLine();
+//        String regexEmail = "^[a-zA-Z][\\\\w-]+@([\\\\w]+\\\\.[\\\\w]+|[\\\\w]+\\\\.[\\\\w]{2,}\\\\.[\\\\w]{2,})$";
+//        if (Pattern.matches(regexEmail, updateEmail)){
+//            user.setEmail(updateEmail);
+//            flag = false;
+//        }else {
+//            System.out.println("Nhập email đúng định dạng.");
+//        }
+//    }
+//
+//    public void updatePassword(User user){
+//        boolean flag = true;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("- Nhập password cập nhật mới:");
+//        String updatePassword = sc.nextLine();
+//        String regexPassword = "";
+//        if (Pattern.matches(regexPassword, updatePassword)){
+//            user.setPassword(updatePassword);
+//            flag = false;
+//        }else {
+//            System.out.println("Nhập mật khẩu dài từ 7 đến 15 ký tự, chứa ít nhất 1 ký tự in hoa, 1 ký tự đặc biệt");
+//        }
+//    }
+}
